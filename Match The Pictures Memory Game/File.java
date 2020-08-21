3c4554c39cb9f553094676812f8e8dd693823f72
PriyaPilla4 <65250086+PriyaPilla4@users.noreply.github.com>
2020-08-21 15:38:51 -0500

    Create File.java

---
+++ Match The Pictures Memory Game/File.java
@@ -0,0 +1,17 @@
+package ppillaproj6;
+
+public class File  { //Tester Class
+
+	public static void main(String[] args) {
+		
+	
+		StoreImages storeImages = new StoreImages();
+		storeImages.addBackOfCard();
+		storeImages.addImages();
+		storeImages.frame();
+		
+
+	}
+
+
+}
